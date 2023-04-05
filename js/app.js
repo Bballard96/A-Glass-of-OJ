046a162e23a83e882a46890381e1cd22894b9d13
Brendan96 <Brendan.ballard96@yahoo.com>
2023-04-05 12:57:56 -0500

    Text changes for grammar errors

--- js/app.js
+++ js/app.js
@@ -1,10 +1,3 @@
-/*-------------------------------- Constants --------------------------------*/
-
-
-
-
-/*---------------------------- Variables (state) ----------------------------*/
-
 let state = {}
 let itemIcon = document.getElementById("item-icon");
 let introOverlay = document.getElementById("intro-overlay");
@@ -15,6 +8,8 @@ const startSound = document.getElementById("StartButton")
 const startNoise = new Audio("../audio/startGame.mp3")
 const optionSound = document.querySelector(".btn")
 const optionNoise = new Audio("../audio/skill.mp3")
+const introSound = new Audio("../audio/pouring.mp3")
+
 
 
 let backgroundState 
@@ -23,25 +18,22 @@ let backgroundState
 startButton.addEventListener("click", function() {
   introOverlay.style.display = "none";
   gameContainer.style.display = "block";
-  startNoise.volume = .2
+  startNoise.volume = .05
   startNoise.play()
+  introSound.volume = .003
+  introSound.play()
 })
-/*------------------------ Cached Element References ------------------------*/
+
 const textElement = document.getElementById('text')
 const optionButtonsElement = document.getElementById('option-buttons')
-/*----------------------------- Event Listeners -----------------------------*/
-/*-------------------------------- Functions --------------------------------*/
-/* START GAME */
+
+
 function startGame() {
 state = {}
 showTextNode(1)
 backgroundState = document.body.style.backgroundImage = "url('../assets/XDjgmZ.png')";
-// iconState = document.body.style = "url('../assets/backpackFav.ico')";
 }
-// document.body.style.backgroundImage = "url('../assets/XDjgmZ.png')"
-// document.body.style.backgroundImage = "url('../assets/dark-forest.jpg')"
-// document.body.style.backgroundImage = "url('../assets/stream-pic.png')"
-// document.body.style.backgroundImage = "url('../assets/bar-pic.png')"
+
 function showTextNode(textNodeIndex) {
   const textNode = textNodes.find(textNode => textNode.id === textNodeIndex)
   textElement.innerText = textNode.text
@@ -57,7 +49,7 @@ function showTextNode(textNodeIndex) {
       optionButtonsElement.appendChild(button)
     }
   })
-  }
+}
 function showOption(option) {
   return option.requiredState == null || option.requiredState(state)
 }
@@ -69,19 +61,19 @@ function selectOption(option) {
   state = Object.assign(state, option.setState)
   showTextNode(nextTextNodeId)
   optionNoise.volume = .05
-    optionNoise.play()
+  optionNoise.play()
 }
 const textNodes = [
-{
+  {
     id: 1,
-    text: "You slowly begin to regain consciousness and awake from your drunken stupor. You arise from the floor, and after rubbing your eyes and squinting intensely, you immediately pat all of your pockets naturally and realize you've lost your wallet. You need to find it before it's too late. You also realize your very parch. For the moment this takes priority. What do you grab to quench your thirst?",
+    text: "You slowly begin to regain consciousness and awake from your drunken stupor. You arise from the floor, and after rubbing your eyes and squinting intensely, you immediately pat all your pockets naturally and realize you've lost your wallet. You should find it before it's too late. You also realize your very parch. For the moment, this takes priority. What do you grab to quench your thirst?",
     options: [
       {
-        text: 'Water.',
+        text: 'Water',
         nextText: 2,
       },
       {
-        text: 'Orange juice.',
+        text: 'Orange juice',
         setState: { orangeJuice: true },
         nextText: 2.2
       },
@@ -93,46 +85,45 @@ const textNodes = [
   },
   // STEP 2
   {
-    id: 2 /*FIRST TIME COMING TO STEP 2 WITH OJ*/,
-    // document.body.style.backgroundImage = "url('../assets/stream-pic.png')",
-    text: "Ah refreshing! Guessing you should get up, get out and see if you can retrace your steps. It Looks like your still in your clothes from yesterday. You should shower and put on something new what'll it be?",
+    id: 2,
+    text: "Ah refreshing! Guessing you should get up, get out and see if you can retrace your steps. It Looks like you are still in your clothes from yesterday. You should shower and put on something new what'll it be?",
     options: [ 
       {
-      text: 'Check weather by stepping outside first.',
+      text: 'Check weather by stepping outside first',
       nextText: 2.1
     },
     {
-      text: 'Shorts and T-shirt.',
+      text: 'Shorts and T-shirt',
       setState: {Cold: true },
       nextText: 2.7
     },
     { 
-      text: 'Pants and jacket.',
+      text: 'Pants and jacket',
       nextText: 4,
     }
     ]
   },
   {
-    id: 2.2 /*FIRST TIME COMING TO STEP 2*/,
+    id: 2.2,
     text: "Ah refreshing! You can never go wrong with some good ol' Oj. Guessing you should get up, get out and see if you can retrace your steps, although it looks like you're still in your clothes from yesterday. You should shower and put on something new what'll it be?",
     options: [ 
       {
-      text: 'Check weather by stepping outside first.',
+      text: 'Check weather by stepping outside first',
       nextText: 2.1
     },
     {
-      text: 'Shorts and T-shirt.',
+      text: 'Shorts and T-shirt',
       setState: {Cold: true },
       nextText: 2.7
     },
     {
-      text: 'Pants and jacket.',
+      text: 'Pants and jacket',
       nextText: 4
     }
     ]
   },
   {
-    id: 2.1, /*MESSAGE AFTER CHECKING WEATHER*/
+    id: 2.1,
     text: "Sheesshh It's cold out!",
     options: [
       {
@@ -147,15 +138,15 @@ const textNodes = [
     ]
   },
   {
-    id: 2.5 /*AFTER CHECKING WEATHER*/,
+    id: 2.5,
     text: "What'll it be?",
     options: [ 
     {
-      text: 'Shorts and T-shirt.',
+      text: 'Shorts and T-shirt',
       nextText: 2.7
     },
     {
-      text: 'Pants and jacket.',
+      text: 'Pants and jacket',
       nextText: 4
     }
     ]
@@ -171,7 +162,7 @@ const textNodes = [
     ]
   },
   {
-    id: 50, /* First ENDING */
+    id: 50,
     text: 'You stumble around the room until you lose your footing and fall onto the floor, Only to have to start the day again, maybe tomorrow, and hope for a better outcome.',
     options: [
       {
@@ -186,33 +177,33 @@ const textNodes = [
     text: "Great! Guess it's time to get going. Upon stepping outside, you realize either the sun has exploded everything shrouded in darkness, or it's very late at night. There are a few different paths you could take." ,
     options: [
       {
-      text: 'Walk East.',
+      text: 'Walk East',
       nextText: 5
       },
       {
-        text: 'Walk North.',
+        text: 'Walk North',
         nextText: 6
       },
       {
-        text: 'Walk South.',
+        text: 'Walk South',
         nextText: 7
       }
     ]
   },
   {
     id: 4.1,
-    text: "There are a few different paths you could take." ,
+    text: "There are a few different paths you could take" ,
     options: [
       {
-      text: 'Walk East.',
+      text: 'Walk East',
       nextText: 5
       },
       {
-        text: 'Walk North.',
+        text: 'Walk North',
         nextText: 6
       },
       {
-        text: 'Walk South.',
+        text: 'Walk South',
         nextText: 7
       },
     ]
@@ -223,23 +214,22 @@ const textNodes = [
     text: "Ahead sits a large forest cloaked in darkness. You recognize this forest. You could maybe get through, but you will need some light." ,
     options: [
       {
-      text: 'Use a flashlight to illuminate the path ahead and proceed down.',
+      text: 'Use a flashlight to illuminate the path ahead and proceed down',
       requiredState: (currentState) => currentState.flashLight,
       nextText: 8
       },
       {
-        text: 'Walk back to path.',
+        text: 'Walk back to path',
         nextText: 4.1
       },
     ]
   },
   {
     id: 6,
-    text: 'You walk along the path for a minute or two before approaching what seems to be a pillar with a large handle. ',
+    text: 'You walk along the path for a minute or two before approaching a single large tree with a large handle. You approach the tree to get a closer look.',
     options: [
       {
         text: 'Pull handle?',
-        // requiredState: (currentState) => currentState.orangeJuice,
         nextText: 6.1
       },
       {
@@ -250,7 +240,7 @@ const textNodes = [
   },
   {
     id: 6.1,
-    text: "You pull the handle and reveal a compartment. There's a flashlight inside! Do you take it?",
+    text: "You pull the handle and reveal a hidden compartment inside the tree. There's a flashlight inside! Do you take it?",
     options: [
       {
         text: "Of course, I will take something that most likely belongs to someone else and make it my own!",
@@ -269,7 +259,7 @@ const textNodes = [
     text: 'It looks like you stumble upon a small stream with something shiny beneath the surface.',
     options: [
       {
-        text: "Reach down and pick up whatever item this is.",
+        text: "Reach down and pick up whatever item this is",
         nextText: 7.2,
       },
       {
@@ -285,14 +275,14 @@ const textNodes = [
   },
   {
     id: 7.2,
-    text: "It's a flask filled with your favorite bourbon. ",
+    text: "It's a flask filled with your favorite bourbon.",
     options: [
       {
-        text: "Drink every last drop of strangers alcohol that we found at the bottom of this river. A great idea!",
+        text: "Consume the rest of the strangers' alcohol that we found at the bottom of this river. A great idea!",
         nextText: 50.1
       },
       {
-        text: "I Don't want to take my chances on whatever else could be in that water. I'm Going to head back to the path.",
+        text: "You decide to show an impressive amount of self-restraint, place the flask back where you got it from and head back to the path",
         nextText: 4.1
       },
     ]
@@ -302,14 +292,14 @@ const textNodes = [
     text: "You start to walk through the forest using the flashlight to illuminate in front of you. It seems you're approaching a building.",
     options: [
       {
-        text: "Approach building.",
+        text: "Approach building",
         nextText: 9
       },
     ]
   },
   {
     id: 9,
-    text: "You get closer to the building and realize there is music playing from the inside, and it looks like there could be others inside. ",
+    text: "You get closer to the building and realize there is music playing from the inside, and it looks like there could be others inside.",
     options: [
       {
         text: "My wallet could be in here. I'll go inside and check it out.",
@@ -323,7 +313,7 @@ const textNodes = [
   },
   {
     id: 10,
-    text: `You step inside, and it seems we've arrived at a bar. We approach the bar to sit down. It's all coming back to you now! The bartender turns around, notices you, and as he's cleaning his glass, stops and says: "Hey I recognize you! You left your wallet last night."`,
+    text: `You step inside, and it seems we've arrived at a bar. We approach the bar to sit down. It's all coming back to you now! The bartender turns around, notices you, and as he's cleaning his glass, stops and says: "Hey, I recognize you! You left your wallet last night."`,
     options: [
       {
         text: `*Grab Wallet*`,
@@ -333,7 +323,7 @@ const textNodes = [
   },
   {
     id: 11,
-    text: `"Look We can't have another night like last night so take it easy today if you are thinking about drinking. What'll it be?"`,
+    text: `"Look, we can't have another night like last night, so take it easy today if you are thinking about drinking. What'll it be?"`,
     options: [
       {
         text: `Take his advice to heart. "You're right. I'll just have a glass of water and be on my way."`,
@@ -344,7 +334,7 @@ const textNodes = [
         nextText: 50.2
       },
       {
-      text: "Yeah you're right. I actually just had some orange juice this morning still working on getting rid of this hangover. I'll just take a glass of Oj. It's my favorite.",
+      text: "Yeah, you're right. I just had some orange juice this morning still working on getting rid of this hangover. I'll take a glass of Oj. It's my favorite.",
       requiredState: (currentState) => currentState.orangeJuice,
       nextText: 60,
       }
@@ -352,7 +342,7 @@ const textNodes = [
   },
   {
     id: 12,
-    text: "You have proven yourself to be very responsible and mature with this decision. Great job",
+    text: "You have proven yourself to be very responsible and mature with this decision. Great job!",
     options: [
       {
         text: "You Win! Restart.",
@@ -372,7 +362,7 @@ const textNodes = [
   },
   {
     id: 50.2,
-    text: "As everything begins to get fuzzy and you leave it up to the bar staff and ongoers to decide how they're going to get rid of you once again. Right before you collapse after taking one too many drinks you realize you have failed yourself and everyone around you by falling to the temptations of liquor once again and leaving your wallet on the bar counter. ",
+    text: "As everything gets fuzzy,  you leave it up to the bar staff and ongoers to decide how to get rid of you again. Right before you collapse after taking too many drinks you realize you have failed yourself and everyone around you by falling to the temptations of liquor again and leaving your wallet on the bar counter.",
     options: [
       {
         text: "You Lose. Restart",
