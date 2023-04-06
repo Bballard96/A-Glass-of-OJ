da7c2a817715a9c4cf56109ffc773ba04a9a4d4c
Brendan96 <Brendan.ballard96@yahoo.com>
2023-04-06 18:27:17 -0500

    text changes for errors

--- data/storyline.js
+++ data/storyline.js
@@ -4,11 +4,11 @@ const textNodes = [
       text: "You slowly begin to regain consciousness and awake from your drunken stupor. You arise from the floor, and after rubbing your eyes and squinting intensely, you immediately pat all of your pockets naturally and realize you've lost your wallet. You need to find it before it's too late. You also realize your very parch. For the moment this takes priority. What do you grab to quench your thirst?",
       options: [
         {
-          text: 'Water.',
+          text: 'Water',
           nextText: 2,
         },
         {
-          text: 'Orange juice.',
+          text: 'Orange juice',
           setState: { orangeJuice: true },
           nextText: 2.2
         },
@@ -23,16 +23,16 @@ const textNodes = [
       text: "Ah refreshing! Guessing you should get up, get out and see if you can retrace your steps. It Looks like your still in your clothes from yesterday. You should shower and put on something new what'll it be?",
       options: [ 
         {
-        text: 'Check weather by stepping outside first.',
+        text: 'Check weather by stepping outside first',
         nextText: 2.1
       },
       {
-        text: 'Shorts and T-shirt.',
+        text: 'Shorts and T-shirt',
         setState: {Cold: true },
         nextText: 2.7
       },
       { 
-        text: 'Pants and jacket.',
+        text: 'Pants and jacket',
         nextText: 4,
       }
       ]
@@ -42,16 +42,16 @@ const textNodes = [
       text: "Ah refreshing! You can never go wrong with some good ol' Oj. Guessing you should get up, get out and see if you can retrace your steps, although it looks like you're still in your clothes from yesterday. You should shower and put on something new what'll it be?",
       options: [ 
         {
-        text: 'Check weather by stepping outside first.',
+        text: 'Check weather by stepping outside first',
         nextText: 2.1
       },
       {
-        text: 'Shorts and T-shirt.',
+        text: 'Shorts and T-shirt',
         setState: {Cold: true },
         nextText: 2.7
       },
       {
-        text: 'Pants and jacket.',
+        text: 'Pants and jacket',
         nextText: 4
       }
       ]
@@ -76,11 +76,11 @@ const textNodes = [
       text: "What'll it be?",
       options: [ 
       {
-        text: 'Shorts and T-shirt.',
+        text: 'Shorts and T-shirt',
         nextText: 2.7
       },
       {
-        text: 'Pants and jacket.',
+        text: 'Pants and jacket',
         nextText: 4
       }
       ]
@@ -111,33 +111,33 @@ const textNodes = [
       text: "Great! Guess it's time to get going. Upon stepping outside, you realize either the sun has exploded everything shrouded in darkness, or it's very late at night. There are a few different paths you could take." ,
       options: [
         {
-        text: 'Walk East.',
+        text: 'Walk East',
         nextText: 5
         },
         {
-          text: 'Walk North.',
+          text: 'Walk North',
           nextText: 6
         },
         {
-          text: 'Walk South.',
+          text: 'Walk South',
           nextText: 7
         }
       ]
     },
     {
       id: 4.1,
-      text: "There are a few different paths you could take." ,
+      text: "There are a few different paths you could take" ,
       options: [
         {
-        text: 'Walk East.',
+        text: 'Walk East',
         nextText: 5
         },
         {
-          text: 'Walk North.',
+          text: 'Walk North',
           nextText: 6
         },
         {
-          text: 'Walk South.',
+          text: 'Walk South',
           nextText: 7
         },
       ]
@@ -153,14 +153,14 @@ const textNodes = [
         nextText: 8
         },
         {
-          text: 'Walk back to path.',
+          text: 'Walk back to path',
           nextText: 4.1
         },
       ]
     },
     {
       id: 6,
-      text: 'You walk along the path for a minute or two before approaching what seems to be a pillar with a large handle. ',
+      text: 'You walk along the path for a minute or two before approaching a single large tree with a large handle.',
       options: [
         {
           text: 'Pull handle?',
@@ -193,7 +193,7 @@ const textNodes = [
       text: 'It looks like you stumble upon a small stream with something shiny beneath the surface.',
       options: [
         {
-          text: "Reach down and pick up whatever item this is.",
+          text: "Reach down and pick up whatever item this is",
           nextText: 7.2,
         },
         {
@@ -201,7 +201,7 @@ const textNodes = [
           nextText: 4.1
         },
         {
-          text: "Use your flashlight to light the water and stare at yourself before returning to the path.",
+          text: "Use your flashlight to light the water and stare at yourself before returning to the path",
           requiredState: (currentState) => currentState.flashLight,
           nextText: 4.1
         },
@@ -209,7 +209,7 @@ const textNodes = [
     },
     {
       id: 7.2,
-      text: "It's a flask filled with your favorite bourbon. ",
+      text: "It's a flask filled with your favorite bourbon.",
       options: [
         {
           text: "Drink every last drop of strangers alcohol that we found at the bottom of this river. A great idea!",
@@ -226,7 +226,7 @@ const textNodes = [
       text: "You start to walk through the forest using the flashlight to illuminate in front of you. It seems you're approaching a building.",
       options: [
         {
-          text: "Approach building.",
+          text: "Approach building",
           nextText: 9
         },
       ]
@@ -276,7 +276,7 @@ const textNodes = [
     },
     {
       id: 12,
-      text: "You have proven yourself to be very responsible and mature with this decision. Great job",
+      text: "You have proven yourself to be very responsible and mature with this decision. Great job.",
       options: [
         {
           text: "You Win! Restart.",
@@ -299,7 +299,7 @@ const textNodes = [
       text: "As everything begins to get fuzzy and you leave it up to the bar staff and ongoers to decide how they're going to get rid of you once again. Right before you collapse after taking one too many drinks you realize you have failed yourself and everyone around you by falling to the temptations of liquor once again and leaving your wallet on the bar counter. ",
       options: [
         {
-          text: "You Lose. Restart",
+          text: "You Lose. Restart.",
           nextText: -1
         },
       ]
@@ -309,7 +309,7 @@ const textNodes = [
       text: "A glass of OJ. Can never go wrong.",
       options: [
         {
-          text: "You Win! Restart",
+          text: "You Win! Restart.",
           nextText: -1
         },
       ]
